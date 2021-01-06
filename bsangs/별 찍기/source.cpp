8232b94cefe00dd691c9e080867c58c76ed13bdf
bsangs <49545238+bsangs@users.noreply.github.com>
2021-01-06 22:17:49 +0900

    feat: code design

--- bsangs/별 찍기/source.cpp
+++ bsangs/별 찍기/source.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-bool triangle_draw[3072][6145];
+bool triangle_draw[6145][3072];
 
 int get_height(int k) {
 	int height = 2;
@@ -22,8 +22,21 @@ int get_k(int N) {
 	return k;
 }
 
-void triangle_print(int n, int x, int y) {
-	
+void triangle_mapping(int triangle_count, int x, int y) {
+	/*
+		실질적 재귀함수
+		triangle_count: 세로로 삼각형을 쌓을 수 있는 양 (1이면 가장 깊은 depth)
+		x: x offset
+		y: y offset
+	*/
+
+	if (triangle_count == 1) {
+		triangle_draw[x + 2][y] = true;
+		triangle_draw[x + 1][y + 1] = true;
+		triangle_draw[x + 3][y + 1] = true;
+		for (int i = 0; i < 5; i++) triangle_draw[x + i][y + 2] = true;
+		return;
+	}
 }
 
 void print_triangle(int width, int height) {
@@ -36,12 +49,20 @@ void print_triangle(int width, int height) {
 	}
 }
 int main() {
-	int N, k, height, width;
+	int N, triangle_height, height, width;
 	
 	scanf("%d", &N);
 	
+	// 출력할 때 쓰는 높이
 	height = N;
+
+	// 출력할 대 쓰는 폭
 	width = N * 2 - 1;
+
+	// triangle_mapping 에서 용도 설명
+	triangle_count = N / 3;
+	
+	triangle_mapping(triangle_count, 0, 0)
 	
 	print_triangle(width, height);
 }
