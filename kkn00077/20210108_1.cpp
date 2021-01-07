f49b29eb19dfc5444ec0dfa924b9f4ffbe560e48
GYUNA KIM <kkn00077@gmail.com>
2021-01-07 21:07:13 +0900

    달팽이 머시기 어쩌구...

---
+++ kkn00077/20210108_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int get_climb_day(int, int, int);
+
+int main()
+{
+    int a, b, v;
+    
+    cin >> a >> b >> v;
+    cout << get_climb_day(a, b, v)<<endl;
+    
+    return 0;
+}
+
+int get_climb_day(int a, int b, int v){
+    int total_h = v-b; // 마지막 날에도 밤에 b만큼 떨어지니까
+    int one_day_h = a-b; // 하루에 최종적으로 올라가는 높이
+    int days = total_h/one_day_h;
+    return (total_h % one_day_h != 0)?(++days):(days);
+}
