1f5ad2e81efca319c4db0733606d957ac79c915b
GYUNA KIM <kkn00077@gmail.com>
2021-01-08 10:01:31 +0900

    주석을 잘못 써서 수정

--- kkn00077/20210108_1.cpp
+++ kkn00077/20210108_1.cpp
@@ -14,7 +14,7 @@ int main()
 }
 
 int get_climb_day(int a, int b, int v){
-    int total_h = v-b; // 마지막 날에도 밤에 b만큼 떨어지니까
+    int total_h = v-b; // 마지막 날에는 b만큼 떨어지지 않는다
     int one_day_h = a-b; // 하루에 최종적으로 올라가는 높이
     int days = total_h/one_day_h;
     return (total_h % one_day_h != 0)?(++days):(days);
